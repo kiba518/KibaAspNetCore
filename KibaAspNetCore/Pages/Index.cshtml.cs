978df5813367436152b8402cf3e2921deb6f23c6
kiba518 <jinxuanfeng518@163.com>
2019-04-19 13:38:01 +0800

    KibaAspNetCore
    KibaAspNetCore

---
+++ KibaAspNetCore/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KibaAspNetCore.Pages
+{
+    public class IndexModel : PageModel
+    {
+        //http://localhost:19492/index?title=kiba
+        public void OnGet()
+        { 
+            string title = this.Request.Query["title"];
+            if (!string.IsNullOrWhiteSpace(title))
+            {
+                ViewData["Title"] = title;
+            }
+        }
+    }
+}
